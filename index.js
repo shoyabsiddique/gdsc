762fb6652013dcfc6d683abce678ebeff8981781
Shoyab Siddique <shoyabsiddique692@gmail.com>
2024-02-02 23:40:39 +0530

    firebase setup

--- index.js
+++ index.js
@@ -18,4 +18,5 @@ const firebaseConfig = {
 
 // Initialize Firebase
 const app = initializeApp(firebaseConfig);
-const analytics = getAnalytics(app);
+console.log(app.name);
+// const analytics = getAnalytics(app);
